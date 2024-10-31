a39db0b2b7b0bbe190719abc81d206fa6207d69b
cpalmer546 <cpalmer5462@gmail.com>
2024-10-30 22:33:44 -0500

    Updated name

--- MarkDownParser.py
+++ MarkDownParser.py
@@ -2,6 +2,7 @@
 Used to parse a markdown file for keywords or phrases
 returns a list of words and sentences
 """
+import sys
 
 
 class MarkDownParser:
@@ -9,7 +10,33 @@ class MarkDownParser:
         self.path_to_file = path_to_file
 
         # This will return a list to be used in the FileScanner
-        self.list_to_return: list
+        self.list_to_return = []
+
+    def remove_chars(self, char):
+        item_counter = 0
+        for item in self.list_to_return:
+            if item.__contains__(f"{char}"):
+                item = item.replace(f"{char}", "")
+                self.list_to_return[item_counter] = item
+            item_counter += 1
 
     def parse_file(self):
-        pass
+        try:
+            with open(self.path_to_file, "r") as md_file:
+                self.list_to_return = md_file.readlines()
+                md_file.close()
+        except:
+            print("Not able to import file. Exiting...")
+            sys.exit()
+
+        # Gets rid of \n and \t in list
+        item_counter = 0
+        for item in self.list_to_return:
+            if item == "\n":
+                self.list_to_return.pop(item_counter)
+            item_counter += 1
+
+        self.remove_chars("\n")
+        self.remove_chars("\t")
+
+        return self.list_to_return
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ The main file that will handle the input for the file scanner and provide the ou
 """
 import sys
 from FileScanner import FileScanner
+from MarkDownParser import MarkDownParser
 
 
 def display_text(text, level):
@@ -21,9 +22,11 @@ if __name__ == "__main__":
 
     try:
         base_directory = sys.argv[1]
+        path_to_md_file = sys.argv[2]
 
     except IndexError:
         base_directory = None
+        path_to_md_file = None
         pass
 
     word_list = []
@@ -31,5 +34,14 @@ if __name__ == "__main__":
     if base_directory is None:
         display_text("Base directory not found", 2)
         base_directory = input("Please enter the full file path to your obsidian folder: ")
+
+    if path_to_md_file is None:
+        display_text("MD file not found", 2)
+        path_to_md_file = input("Please enter the full file path to your md file to parse: ")
+
+    mdp = MarkDownParser(path_to_md_file)
+
+    word_list = mdp.parse_file()
+    print(word_list)
 
     fs = FileScanner(base_directory, word_list)
